f80547395f19930899c5df6bbf032035fb720316
priyanshu <priyanshuchoudhary0104@gmail.com>
2023-12-28 01:42:36 -0500

    Usual commit after changes

--- src/component/HomePage/TodoList/TodoList.tsx
+++ src/component/HomePage/TodoList/TodoList.tsx
@@ -1,19 +1,39 @@
 import React, { useState } from 'react'
 
-function TodoList() {
+const TodoList: React.FC = () => {
 
-    // const [todo, setTodo] = useState([])
+    interface Item {
+        id: number;
+        text: string;
+        completed: boolean;
+    }
+
+    const [input, setInput] = useState([])
+
+    const [todos, setTodos] = useState<Item[]>([
+        { id: 1, text: 'Clean Room', completed: false },
+        { id: 2, text: 'Go for Walk', completed: false }
+    ]);
+
+    const handleClick = () => {
+        if (input.trim() !== '') {
+            const newTodos: Item = { id: Date.now(), text: input, completed: false }
+            setTodos([...todos, newTodos]);
+            setInput('');
+        }
+    };
+
     return (
         <>
             <div className='heading-containers'>
                 <h2>Todo-List by Priyanshu</h2>
             </div>
+
             <div className='main-container'>
-                <input type='text' />
-                <button>Add</button>
+                <input type='text' value={input} />
+                <button onClick={handleClick}>Add</button>
             </div>
         </>
-
     )
 }
 
